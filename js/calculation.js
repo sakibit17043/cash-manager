e29fa23f69c0149db0dc5a384526ff14c3f9a858
sakibit17043 <it17043@mbstu.ac.bd>
2022-02-17 21:23:50 +0600

    project done

--- js/calculation.js
+++ js/calculation.js
@@ -1,35 +1,56 @@
 function getInputValue(idName){
    const inputValueText= document.getElementById(idName).value;
+   // for empty field error handeling 
+   if(inputValueText=="")
+   return "empty";
    const inputValueAmount =parseFloat(inputValueText);
-   
-   return inputValueAmount;
+    return inputValueAmount;
 }
 // calculate button event handle
 document.getElementById("calculate-button").addEventListener("click",function(){
    const incomeValue =getInputValue("income-input");
-   let isnegative =false;
+   let isEmpty =false;
+   if(incomeValue=="empty"){
+   alert("please put a value in income field");
+   isEmpty =true;
+   }
+   // for negative number error handeling 
+   let isNegative =false;
    if(incomeValue<0){
    alert("please give a positive income value");
-   isnegative =true;
+   isNegative =true;
    }
    const foodValue =getInputValue("food-input");
+   if(foodValue=="empty"){
+      alert("please put a value in food field");
+      isEmpty =true;
+      }
    if(foodValue<0){
    alert("please give a positive food value");
-   isnegative =true;
+   isNegative =true;
    }
    const rentValue =getInputValue("rent-input");
+   if(rentValue=="empty"){
+      alert("please put a value in rent field");
+      isEmpty =true;
+      }
    if(rentValue<0){
    alert("please give a positive rent value");
-   isnegative =true;
+   isNegative =true;
    }
    const clothesValue =getInputValue("clothes-input");
+   if(clothesValue=="empty"){
+      alert("please put a value in clothes field");
+      isEmpty =true;
+      }
    if(clothesValue<0){
    alert("please give a positive clothes value");
-   isnegative =true;
+   isNegative =true;
    }
    const totalExpenses =document.getElementById("total-expenses");
    const totalExpensesValue =foodValue+rentValue+clothesValue;
-   if(isnegative==false){
+   if(isNegative==false && isEmpty==false){
+      // income-expense error handeling 
    if(totalExpensesValue>incomeValue)
    alert("You can't expense more than your income");
    else{
@@ -44,28 +65,28 @@ document.getElementById("calculate-button").addEventListener("click",function(){
 document.getElementById("save-button").addEventListener("click",function(){
    
    const savevalueNumber =getInputValue("save-input");
-   let ispositive=true
+   let isSaveEmpty =false;
+   if(savevalueNumber=="empty"){
+      alert("please put a value in income field");
+      isSaveEmpty =true;
+      }
+   let isPositive=true
    if(savevalueNumber<0){
    alert("please give a positive savings value");
-   ispositive=false
+   isPositive=false
    }
-   if(ispositive==true){
+   if(isPositive==true && isSaveEmpty==false){
    const savingPercentage =savevalueNumber/100;
    const incomeAmount =getInputValue("income-input");
-   
-
    const balanceText =document.getElementById("balance").innerText;
    const balanceAmount =parseFloat(balanceText);
-   // saving calculation error handeling 
-   
-   
    const savingAmount =savingPercentage*incomeAmount;
+     // saving calculation error handeling 
    if(savingAmount>balanceAmount)
    alert("You have not enough money for save");
    else{
    document.getElementById("saving-amount").innerText=savingAmount;
       // remaining balance calculation 
-
    const remainingBalanceAmount =balanceAmount-savingAmount;
    document.getElementById("remaining-balance").innerText=remainingBalanceAmount;
    }
