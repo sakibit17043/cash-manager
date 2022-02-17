12f930bbbd4cb15f92ffc17f2001e43a94d60e23
sakibit17043 <it17043@mbstu.ac.bd>
2022-02-17 19:06:14 +0600

    income-expense calculation complete

--- js/calculation.js
+++ js/calculation.js
@@ -1,5 +1,23 @@
 function getInputValue(idName){
-   const inputValue= document.getElementById(idName).value;
-   return inputValue;
+   const inputValueText= document.getElementById(idName).value;
+   const inputValueAmount =parseFloat(inputValueText);
+   return inputValueAmount;
 }
-document.getElementById
+// calculate button event handle
+document.getElementById("calculate-button").addEventListener("click",function(){
+   const incomeValue =getInputValue("income-input");
+   const foodValue =getInputValue("food-input");
+   const rentValue =getInputValue("rent-input");
+   const clothesValue =getInputValue("clothes-input");
+   const totalExpenses =document.getElementById("total-expenses");
+   totalExpensesValue =foodValue+rentValue+clothesValue;
+   totalExpenses.innerText=totalExpensesValue;
+   // balance calculation 
+   const balance =incomeValue -totalExpensesValue;
+   document.getElementById("balance").innerText=balance;
+
+})
+// save button event handle 
+document.getElementById("save-input").addEventListener("click",function(){
+   const saveValue =document.getElementById("")
+})
